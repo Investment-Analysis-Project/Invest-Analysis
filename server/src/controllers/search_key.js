272d76b8b1e2dabf8b2cf1d748feaa440bef043b
alengeorge-26 <alengeorge261@gmail.com>
2024-02-03 12:25:39 +0530

    ML API returning format

--- server/src/controllers/search_key.js
+++ server/src/controllers/search_key.js
@@ -1,12 +1,16 @@
 const createError = require('../utils/error');
 const axios = require('axios');
+//const fetch = require('node-fetch');
+//http://localhost:5000/api/search_key/apple
+
+const resultArray = [];
 
 const recent_news = async(req,res,next)=>{
     try{
         const {keyword}=req.params;
         
         const response = await axios.get(`https://newsapi.org/v2/everything?q=${keyword}&searchIn=title&from=2024-01-20&language=en&sortBy=relevancy&apiKey=dd4dcc554dd94d61820961820e342242`);
-        
+
         const five_news = response.data.articles.slice(0,5);
         const newsArray = [];
         five_news.forEach(news => {
@@ -16,6 +20,9 @@ const recent_news = async(req,res,next)=>{
 
         processNewsArray(newsArray);
 
+        if(resultArray.length)
+            console.log(resultArray);
+        console.log("\n\n")
     }catch(err){
         console.log(err);
     }
@@ -49,10 +56,11 @@ const query = async(data,news)=>
 
         const result1 = await response1.json();
         const result2 = await response2.json();
-        console.log(news.title);
-        console.log(result1[0][0]);
-        console.log(result2);
-        console.log("\n\n")
+        
+        const news_title=news.title;
+        const news_sentiment=result1[0][0];
+        const news_entities=result2;
+        resultArray.push({news_title,news_sentiment,news_entities});
     }catch(err){
         console.log(err)
     }
