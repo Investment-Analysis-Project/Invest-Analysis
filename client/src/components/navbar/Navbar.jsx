95f70d578527f111b826b20e081af9b2d9a1d4d4
alengeorge-26 <alengeorge261@gmail.com>
2024-03-18 14:29:06 +0530

    Navbar Changed

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,44 +3,49 @@ import './navbar.css';
 import { ProjectsContext } from '../../contextapi.js/projectscontext';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faHouseUser, faRightFromBracket, faSignIn, faUserCircle, faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import {  faHouseUser, faRightFromBracket, faSignIn, faUserCircle, faUserSecret ,faUser,faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = (props) => {
 
-    const {home,login_page}=props;
+    const {login_page}=props;
 
     const navigate = useNavigate();
-    const [profilevisible,setprofilevisible]=useState(false);
+    const [profilevisible,setprofilevisible]=useState(true);
 
     const profileview=()=>{
         setprofilevisible(!profilevisible);
     }
 
     const {auth,setAuth}=useContext(ProjectsContext);
+    setAuth(false);
 
     return(
         <div className='navbar'>
-            {(!home || auth) && (<div className='items'>
-                <button className='navbut' onClick={()=>{navigate('/')}}><FontAwesomeIcon icon={faHouseUser} /> Home</button>  
-            </div>)}
+            <div className="nav_container">
+                <div className='logo'>
+                    <span onClick={()=>{navigate('/')}}>InvestAnalysis.</span>  
+                </div>
 
-            {auth ? (<div className='items'>
-                <button className='navbut' onClick={profileview}><FontAwesomeIcon icon={faUserCircle} size='xl'/></button>
+                {auth ? (<div className='items'>
+                    <button className='navbut' onClick={()=>{navigate('/login')}}>Search For Company&nbsp;&nbsp; <FontAwesomeIcon icon={faSearch} style={{color: "#ffffff",}} size="sm"/></button>
 
-                {profilevisible && <div className='logindropdown'>
-                    <ul className='liststyle'>
-                        <li>
-                           <p className='profilelistitem' onClick={()=>{navigate('/')}}><FontAwesomeIcon icon={faUserSecret}/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Profile</p>  
-                        </li>
-                        <li>
-                            <p className='profilelistitem' onClick={()=>{setAuth(false); localStorage.removeItem('token');navigate('/')}}><FontAwesomeIcon icon={faRightFromBracket}/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Logout</p>
-                        </li>
-                    </ul>
-                </div>}
-            </div>) :
-            (!login_page && <div className='items'>
-                <button className='navbut' onClick={()=>{navigate('/login')}}><FontAwesomeIcon icon={faSignIn}/> Login</button>
-            </div>)}
+                    <button className='navbut' onClick={profileview}><FontAwesomeIcon icon={faUser} style={{color: "#ffffff",}} size="xl"/></button>
+
+                    {profilevisible && <div className='logindropdown'>
+                        <ul className='liststyle'>
+                            <li>
+                            <p className='profilelistitem' onClick={()=>{navigate('/')}}>Profile</p>  
+                            </li>
+                            <li>
+                                <p className='profilelistitem' onClick={()=>{setAuth(false); localStorage.removeItem('token');navigate('/')}}>Logout</p>
+                            </li>
+                        </ul>
+                    </div>}
+                </div>) :
+                (!login_page && <div className='items'>
+                    <button className='navbut' onClick={()=>{navigate('/login')}}>Login</button>
+                </div>)}
+            </div>
         </div>
     );
 }
