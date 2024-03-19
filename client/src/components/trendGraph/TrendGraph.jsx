7a4dfabbc9b82117c86195523492a784bf847c3d
alengeorge-26 <alengeorge261@gmail.com>
2024-03-20 00:35:59 +0530

    Dashboard Changed

--- client/src/components/trendGraph/TrendGraph.jsx
+++ client/src/components/trendGraph/TrendGraph.jsx
@@ -7,7 +7,7 @@ const TrendGraph = () => {
         id="google-trends-iframe"
         title="Google Trends Widget"
         src="https://trends.google.com:443/trends/embed/explore/TIMESERIES?req=%7B%22comparisonItem%22%3A%5B%7B%22keyword%22%3A%22pepsi%22%2C%22geo%22%3A%22IN%22%2C%22time%22%3A%22now%201-d%22%7D%5D%2C%22category%22%3A0%2C%22property%22%3A%22%22%7D&tz=-330&hl=en-GB"
-        width="500"
+        width="100%"
         height="350"
       ></iframe>
     );
--- client/src/pages/result/Result.jsx
+++ client/src/pages/result/Result.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './result.css';
 import { useNavigate} from 'react-router-dom';
 import { useEffect,useRef} from 'react';
-import Navbar from '../../components/navbar/Navbar';
+import img from '../../components/news/img.jpg'
 import TrendGraph from '../../components/trendGraph/TrendGraph';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faBars } from '@fortawesome/free-solid-svg-icons';
@@ -14,40 +14,32 @@ const Result = () => {
   
   return (
     <div>
-      <Navbar result={true}/>
-
       <div className="result_page">
       <div className="result_container">
         <aside id="sidebar">
           <div class="sidebar-title">
             <div class="sidebar-brand">
-              <span class="material-icons-outlined">Username</span> 
+              <span onClick={()=>{navigate('/')}}>InvestAnalysis.</span>  
             </div>
             <span class="material-icons-outlined" onclick="closeSidebar()">close</span>
           </div>
 
           <ul class="sidebar-list">
             <li class="sidebar-list-item">
-              <a href="#">
                 <span class="material-icons-outlined">Profile</span>
-              </a>
             </li>
+
             <li class="sidebar-list-item">
-              <a href="#">
+              
                 <span class="material-icons-outlined">History</span>
-              </a>
             </li>
             
             <li class="sidebar-list-item">
-              <a href="#">
                 <span class="material-icons-outlined">Settings</span>
-              </a>
             </li>
           
-            <li class="sidebar-list-item">
-              <a href="#">
+            <li class="sidebar-list-item"> 
                 <span class="material-icons-outlined">Logout</span>
-              </a>
             </li>
           </ul>
         </aside>
@@ -66,9 +58,67 @@ const Result = () => {
           
           <main class="main-container">
             <div class="main-title">
-              <p class="font-weight-bold">Results</p>
+              <p class="font-weight-bold">Top Results</p>
+            </div>
+            <div className="newscontainer">
+              <div className='newsitem' id="newspointer">
+                  <span className='news-title'>Anker power banks and wireless chargers are up to 43 percent off for the Amazon Spring Sale</span>
+                  <img src="https://img.freepik.com/free-vector/banking-business-facebook-template_23-2150971682.jpg?t=st=1710874848~exp=1710878448~hmac=440ae8aa2ab1ba105fa057be926c1c73e10a2abc25a20c6f23b6878d0461f870&w=900" alt="" />
+                  <div className='news-source'>
+                    <span>CNBC</span>
+                    <span>26-11-2024 10:30 PM</span>
+                  </div>
+              </div>
+
+              <div className='newsitem' id="newspointer">
+                  <span className='news-title'>Anker power banks and wireless chargers are up to 43 percent off for the Amazon Spring Sale</span>
+                  <img src="https://img.freepik.com/free-vector/banking-business-facebook-template_23-2150971682.jpg?t=st=1710874848~exp=1710878448~hmac=440ae8aa2ab1ba105fa057be926c1c73e10a2abc25a20c6f23b6878d0461f870&w=900" alt="" />
+                  <div className='news-source'>
+                    <span>CNBC</span>
+                    <span>26-11-2024 10:30 PM</span>
+                  </div>
+              </div>
+
+              <div className='newsitem' id="newspointer">
+                  <span className='news-title'>Anker power banks and wireless chargers are up to 43 percent off for the Amazon Spring Sale</span>
+                  <img src="https://img.freepik.com/free-vector/banking-business-facebook-template_23-2150971682.jpg?t=st=1710874848~exp=1710878448~hmac=440ae8aa2ab1ba105fa057be926c1c73e10a2abc25a20c6f23b6878d0461f870&w=900" alt="" />
+                  <div className='news-source'>
+                    <span>CNBC</span>
+                    <span>26-11-2024 10:30 PM</span>
+                  </div>
+              </div>
+
+              <div className='newsitem' id="newspointer">
+                  <span className='news-title'>Anker power banks and wireless chargers are up to 43 percent off for the Amazon Spring Sale</span>
+                  <img src="https://img.freepik.com/free-vector/banking-business-facebook-template_23-2150971682.jpg?t=st=1710874848~exp=1710878448~hmac=440ae8aa2ab1ba105fa057be926c1c73e10a2abc25a20c6f23b6878d0461f870&w=900" alt="" />
+                  <div className='news-source'>
+                    <span>CNBC</span>
+                    <span>26-11-2024 10:30 PM</span>
+                  </div>
+              </div>
+
+              <div className='newsitem' id="newspointer">
+                  <span className='news-title'>Anker power banks and wireless chargers are up to 43 percent off for the Amazon Spring Sale</span>
+                  <img src="https://img.freepik.com/free-vector/banking-business-facebook-template_23-2150971682.jpg?t=st=1710874848~exp=1710878448~hmac=440ae8aa2ab1ba105fa057be926c1c73e10a2abc25a20c6f23b6878d0461f870&w=900" alt="" />
+                  <div className='news-source'>
+                    <span>CNBC</span>
+                    <span>26-11-2024 10:30 PM</span>
+                  </div>
+              </div>
+
+              <div className='newsitem' id="newspointer">
+                  <span className='news-title'>Anker power banks and wireless chargers are up to 43 percent off for the Amazon Spring Sale</span>
+                  <img src="https://img.freepik.com/free-vector/banking-business-facebook-template_23-2150971682.jpg?t=st=1710874848~exp=1710878448~hmac=440ae8aa2ab1ba105fa057be926c1c73e10a2abc25a20c6f23b6878d0461f870&w=900" alt="" />
+                  <div className='news-source'>
+                    <span>CNBC</span>
+                    <span>26-11-2024 10:30 PM</span>
+                  </div>
+              </div>
             </div>
 
+            <div class="main-title">
+              <p class="font-weight-bold">Google Trends Search</p>
+            </div>
             <div class="main-cards">
               <div className="graph" >
                   <TrendGraph/>
