9ba946d2f33fd71661d65fafbf53ad5300db716e
alengeorge-26 <alengeorge261@gmail.com>
2024-05-03 21:54:06 +0530

    Q

--- client/src/components/news/News.jsx
+++ client/src/components/news/News.jsx
@@ -17,7 +17,7 @@ const News = () => {
             {
                 // dd4dcc554dd94d61820961820e342242
                 // afcd39b2d9c546cc9293d168cee038e7
-                const response = await fetch(`https://newsapi.org/v2/everything?q=apple&excludeDomains=engadget.com&language=en&sortBy=popularit&from=2024-04-01&to=2024-04-10&apiKey=afcd39b2d9c546cc9293d168cee038e7`)
+                const response = await fetch(`https://newsapi.org/v2/everything?domains=cnbc.com&excludeDomains=engadget.com&language=en&sortBy=popularit&apiKey=afcd39b2d9c546cc9293d168cee038e7`)
                 const data = await response.json();
                 const articles = data.articles;
                 setNews(articles);
